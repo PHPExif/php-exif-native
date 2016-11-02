de390fc8cc8cdf1e98d674806b5608ee4f89d94b
Tom Van Herreweghe <tom@theanalogguy.be>
2016-11-02 20:50:00 +0100

    Mapper to map data onto a Metadata object
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ src/Reader/Mapper.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Mapper for mapping data between raw input and Data classes
+ *
+ * @link        http://github.com/PHPExif/php-exif-native for the canonical source repository
+ * @copyright   Copyright (c) 2016 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/PHPExif/php-exif-native/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Native
+ */
+
+namespace PHPExif\Adapter\Native\Reader;
+
+use PHPExif\Common\Adapter\MapperInterface;
+use PHPExif\Common\Data\MetadataInterface;
+use PHPExif\Common\Mapper\ArrayMapper;
+use PHPExif\Common\Mapper\FieldMapperTrait;
+
+/**
+ * Mapper
+ *
+ * Maps the array of exif & iptc data onto the
+ * correct fields of given MetadataInterface object
+ *
+ * @category    PHPExif
+ * @package     Native
+ */
+class Mapper implements MapperInterface, ArrayMapper
+{
+    use FieldMapperTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function map(array $input, MetadataInterface $output)
+    {
+        $this->mapArray($input, $output);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function mapArray(array $input, &$output)
+    {
+        $mappers = $this->getFieldMappers();
+
+        foreach ($mappers as $field => $mapper) {
+            $mapper->mapField($field, $input, $output);
+        }
+    }
+}
---
+++ tests/Reader/MapperTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\PHPExif\Adapter\Native\Reader\Mapper;
+
+use Mockery as m;
+use PHPExif\Adapter\Native\Reader\Mapper;
+use PHPExif\Common\Data\Exif;
+use PHPExif\Common\Data\Iptc;
+use PHPExif\Common\Data\Metadata;
+use PHPExif\Common\Mapper\FieldMapper;
+
+/**
+ * Class: MapperTest
+ *
+ * @see \PHPUnit_Framework_TestCase
+ * @coversDefaultClass \PHPExif\Adapter\Native\Reader\Mapper
+ * @covers ::<!public>
+ */
+class MapperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::map
+     * @group mapper
+     *
+     * @return void
+     */
+    public function testMapForwardsCall()
+    {
+        $input = array();
+        $output = new Metadata(new Exif, new Iptc);
+
+        $mock = m::mock(Mapper::class . '[mapArray]')->makePartial();
+        $mock->shouldReceive('mapArray')
+            ->once()
+            ->with(
+                $input,
+                m::type(Metadata::class)
+            )
+            ->andReturnNull();
+
+        $mock->map($input, $output);
+    }
+
+    /**
+     * @covers ::mapArray
+     * @group mapper
+     *
+     * @return void
+     */
+    public function testMapArrayForwardsCall()
+    {
+        $input = array();
+        $output = new Metadata(
+            new Exif,
+            new Iptc
+        );
+
+        $mapper = new Mapper;
+
+        foreach (array('exif', 'iptc') as $field) {
+            $fieldMapper = m::mock(FieldMapper::class . '[getSupportedFields,mapField]');
+            $fieldMapper->shouldReceive('getSupportedFields')
+                ->once()
+                ->andReturn(array($field));
+            $fieldMapper->shouldReceive('mapField')
+                ->once()
+                ->with(
+                    $field,
+                    $input,
+                    m::type(Metadata::class)
+                )
+                ->andReturnNull();
+
+            $mapper->registerFieldMapper($fieldMapper);
+        }
+
+        $mapper->mapArray($input, $output);
+    }
+}
