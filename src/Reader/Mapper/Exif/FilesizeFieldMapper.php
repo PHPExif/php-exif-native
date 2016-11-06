62caa4ea71e38f72ba6b1ffaa2807a0ea8df28b2
Tom Van Herreweghe <tom@theanalogguy.be>
2016-11-06 15:20:45 +0100

    Filsize support & unit tests
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ src/Reader/Mapper/Exif/FilesizeFieldMapper.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Mapper for mapping data between raw input and a Filesize VO
+ *
+ * @link        http://github.com/PHPExif/php-exif-native for the canonical source repository
+ * @copyright   Copyright (c) 2016 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/PHPExif/php-exif-native/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Native
+ */
+
+namespace PHPExif\Adapter\Native\Reader\Mapper\Exif;
+
+use PHPExif\Common\Data\ExifInterface;
+use PHPExif\Common\Data\ValueObject\Exif\Filesize;
+use PHPExif\Common\Mapper\FieldMapper;
+
+/**
+ * Mapper
+ *
+ * @category    PHPExif
+ * @package     Native
+ */
+class FilesizeFieldMapper implements FieldMapper
+{
+    use GuardInvalidArgumentsTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSupportedFields()
+    {
+        return array(
+            Filesize::class,
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function mapField($field, array $input, &$output)
+    {
+        $this->guardInvalidArguments($field, $input, $output);
+
+        if (!array_key_exists('FileSize', $input)) {
+            return;
+        }
+
+        $filesize = new Filesize($input['FileSize']);
+
+        $output = $output->withFilesize($filesize);
+    }
+}
---
+++ tests/Reader/Mapper/Exif/FilesizeFieldMapperTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\PHPExif\Adapter\Native\Reader\Mapper\Exif;
+
+use Mockery as m;
+use PHPExif\Adapter\Native\Reader\Mapper\Exif\FilesizeFieldMapper;
+use PHPExif\Common\Data\Exif;
+use PHPExif\Common\Data\ValueObject\Exif\Filesize;
+
+/**
+ * Class: FilesizeFieldMapperTest
+ *
+ * @see \PHPUnit_Framework_TestCase
+ * @coversDefaultClass \PHPExif\Adapter\Native\Reader\Mapper\Exif\FilesizeFieldMapper
+ * @covers ::<!public>
+ */
+class FilesizeFieldMapperTest extends BaseFieldMapperTest
+{
+    /**
+     * FQCN of the fieldmapper being tested
+     *
+     * @var mixed
+     */
+    protected $fieldMapperClass = FilesizeFieldMapper::class;
+
+    /**
+     * List of supported fields
+     *
+     * @var array
+     */
+    protected $supportedFields = [
+        Filesize::class,
+    ];
+
+    /**
+     * Valid input data
+     *
+     * @var array
+     */
+    protected $validInput = [
+        'FileSize' => 5345678,
+    ];
+
+    /**
+     * @covers ::mapField
+     * @group mapper
+     *
+     * @return void
+     */
+    public function testMapFieldHasDataInOutput()
+    {
+        $field = reset($this->supportedFields);
+        $output = new Exif;
+        $mapper = new $this->fieldMapperClass();
+
+        $originalData = $output->getFilesize();
+        $mapper->mapField($field, $this->validInput, $output);
+        $newData = $output->getFilesize();
+
+        $this->assertNotSame($originalData, $newData);
+
+        $this->assertInstanceOf(
+            Filesize::class,
+            $newData
+        );
+
+        $this->assertEquals(
+            $this->validInput['FileSize'],
+            (string) $newData
+        );
+    }
+}
