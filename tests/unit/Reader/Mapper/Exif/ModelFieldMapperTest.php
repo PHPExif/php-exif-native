ea0b7cd9d88f30a28bc9eb8c626d6312393bae3a
Tom Van Herreweghe <tom@theanalogguy.be>
2016-11-10 10:50:09 +0100

    Model fieldmapper & unit tests
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ tests/unit/Reader/Mapper/Exif/ModelFieldMapperTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\PHPExif\Adapter\Native\Reader\Mapper\Exif;
+
+use Mockery as m;
+use PHPExif\Adapter\Native\Reader\Mapper\Exif\ModelFieldMapper;
+use PHPExif\Common\Data\Exif;
+use PHPExif\Common\Data\ValueObject\Model;
+
+/**
+ * Class: ModelFieldMapperTest
+ *
+ * @see \PHPUnit_Framework_TestCase
+ * @coversDefaultClass \PHPExif\Adapter\Native\Reader\Mapper\Exif\ModelFieldMapper
+ * @covers ::<!public>
+ */
+class ModelFieldMapperTest extends BaseFieldMapperTest
+{
+    /**
+     * FQCN of the fieldmapper being tested
+     *
+     * @var mixed
+     */
+    protected $fieldMapperClass = ModelFieldMapper::class;
+
+    /**
+     * List of supported fields
+     *
+     * @var array
+     */
+    protected $supportedFields = [
+        Model::class,
+    ];
+
+    /**
+     * Valid input data
+     *
+     * @var array
+     */
+    protected $validInput = [
+        'Model' => 'NIKON D90',
+    ];
+
+    /**
+     * @covers ::mapField
+     * @group mapper
+     *
+     * @return void
+     */
+    public function testMapFieldHasDataInOutput()
+    {
+        $field = reset($this->supportedFields);
+        $output = new Exif;
+        $mapper = new $this->fieldMapperClass();
+
+        $originalData = $output->getModel();
+        $mapper->mapField($field, $this->validInput, $output);
+        $newData = $output->getModel();
+
+        $this->assertNotSame($originalData, $newData);
+
+        $this->assertInstanceOf(
+            Model::class,
+            $newData
+        );
+
+        $this->assertEquals(
+            $this->validInput['Model'],
+            (string) $newData
+        );
+    }
+}
