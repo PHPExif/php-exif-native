fd36b607e7f50c1cf4fd9d48c64f1456db939fad
Tom Van Herreweghe <tom@theanalogguy.be>
2016-11-17 20:55:30 +0100

    CreationDate FieldMapper & unit tests
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ src/Reader/Mapper/Exif/DateTimeFieldMapper.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Mapper for mapping data between raw input and a datetime object
+ *
+ * @link        http://github.com/PHPExif/php-exif-native for the canonical source repository
+ * @copyright   Copyright (c) 2016 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/PHPExif/php-exif-native/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Native
+ */
+
+namespace PHPExif\Adapter\Native\Reader\Mapper\Exif;
+
+use PHPExif\Common\Data\ExifInterface;
+use PHPExif\Common\Mapper\FieldMapper;
+use \DateTimeImmutable;
+
+/**
+ * Mapper
+ *
+ * @category    PHPExif
+ * @package     Native
+ */
+class DateTimeFieldMapper implements FieldMapper
+{
+    use GuardInvalidArgumentsTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSupportedFields()
+    {
+        return array(
+            DateTimeImmutable::class,
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function mapField($field, array $input, &$output)
+    {
+        $this->guardInvalidArguments($field, $input, $output);
+
+        if (!array_key_exists('DateTimeOriginal', $input)) {
+            return;
+        }
+
+        $datetimeOriginal = new DateTimeImmutable($input['DateTimeOriginal']);
+
+        $output = $output->withCreationDate($datetimeOriginal);
+    }
+}
