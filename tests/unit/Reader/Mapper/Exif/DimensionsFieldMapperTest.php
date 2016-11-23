11a4093515a9caa64f69fedb256494be1456dab8
Tom Van Herreweghe <tom@theanalogguy.be>
2016-11-23 12:58:21 +0100

    Refactor DimensionsFieldMapper for Dimensions VO

--- tests/unit/Reader/Mapper/Exif/DimensionsFieldMapperTest.php
+++ tests/unit/Reader/Mapper/Exif/DimensionsFieldMapperTest.php
@@ -5,6 +5,7 @@ namespace Tests\PHPExif\Adapter\Native\Reader\Mapper\Exif;
 use Mockery as m;
 use PHPExif\Adapter\Native\Reader\Mapper\Exif\DimensionsFieldMapper;
 use PHPExif\Common\Data\Exif;
+use PHPExif\Common\Data\ValueObject\Dimensions;
 use PHPExif\Common\Data\ValueObject\Height;
 use PHPExif\Common\Data\ValueObject\Width;
 
@@ -30,8 +31,7 @@ class DimensionsFieldMapperTest extends BaseFieldMapperTest
      * @var array
      */
     protected $supportedFields = [
-        Height::class,
-        Width::class,
+        Dimensions::class,
     ];
 
     /**
@@ -52,97 +52,21 @@ class DimensionsFieldMapperTest extends BaseFieldMapperTest
      *
      * @return void
      */
-    public function testMapFieldHasHeightDataInOutput()
+    public function testMapFieldHasDimensionsDataInOutput()
     {
         $field = reset($this->supportedFields);
         $output = new Exif;
         $mapper = new $this->fieldMapperClass();
 
-        $originalData = $output->getHeight();
+        $originalData = $output->getDimensions();
         $mapper->mapField($field, $this->validInput, $output);
-        $newData = $output->getHeight();
+        $newData = $output->getDimensions();
 
         $this->assertNotSame($originalData, $newData);
 
         $this->assertInstanceOf(
-            Height::class,
+            Dimensions::class,
             $newData
-        );
-
-        $this->assertEquals(
-            $this->validInput['COMPUTED']['Height'],
-            $newData->getValue()
-        );
-    }
-
-    /**
-     * @covers ::mapField
-     * @group mapper
-     *
-     * @return void
-     */
-    public function testMapFieldHasWidthDataInOutput()
-    {
-        $field = end($this->supportedFields);
-        $output = new Exif;
-        $mapper = new $this->fieldMapperClass();
-
-        $originalData = $output->getWidth();
-        $mapper->mapField($field, $this->validInput, $output);
-        $newData = $output->getWidth();
-
-        $this->assertNotSame($originalData, $newData);
-
-        $this->assertInstanceOf(
-            Width::class,
-            $newData
-        );
-
-        $this->assertEquals(
-            $this->validInput['COMPUTED']['Width'],
-            $newData->getValue()
-        );
-    }
-
-    /**
-     * @covers ::mapField
-     * @group mapper
-     *
-     * @return void
-     */
-    public function testMapFieldHasAllDataInOutput()
-    {
-        $output = new Exif;
-        $mapper = new $this->fieldMapperClass();
-
-        $originalWidth = $output->getWidth();
-        $originalHeight = $output->getHeight();
-
-        foreach ($this->supportedFields as $field) {
-            $mapper->mapField($field, $this->validInput, $output);
-        }
-        $newWidth = $output->getWidth();
-        $newHeight = $output->getHeight();
-
-        $this->assertNotSame($originalWidth, $newWidth);
-        $this->assertNotSame($originalHeight, $newHeight);
-
-        $this->assertInstanceOf(
-            Width::class,
-            $newWidth
-        );
-        $this->assertInstanceOf(
-            Height::class,
-            $newHeight
-        );
-
-        $this->assertEquals(
-            $this->validInput['COMPUTED']['Width'],
-            $newWidth->getValue()
-        );
-        $this->assertEquals(
-            $this->validInput['COMPUTED']['Height'],
-            $newHeight->getValue()
         );
     }
 }
